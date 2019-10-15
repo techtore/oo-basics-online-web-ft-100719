f7c20c32c9293782ca4971ce896f1b872e5d35a9
Victoria Hale <vic.hale74@gmail.com>
2019-10-15 02:53:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,7 @@
 # Make your shoe class here!
 class Shoe 
+   attr_accessor :color, :size, :material #give us reader and writer for free
+  attr_reader :brand
   def initialize(brand)
     @brand = brand
   end 
