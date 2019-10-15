f966ce6cd0f05a33483166d1e156b1e8aa70b810
Victoria Hale <vic.hale74@gmail.com>
2019-10-15 02:56:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,9 @@
 # Make your shoe class here!
 class Shoe 
-   attr_accessor :color, :size, :material #give us reader and writer for free
+   attr_accessor :color, :size, :material :condition#give us reader and writer for free
   attr_reader :brand
   def initialize(brand)
     @brand = brand
   end 
-   def condition=()
-  end
+  
 end 
